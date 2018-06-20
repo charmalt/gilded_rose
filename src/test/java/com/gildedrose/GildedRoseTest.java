8b495087d0527e958f30454fe5e65cc5d263026f
Charlene <charlenechetcuti@gmail.com>
2018-06-20 15:53:58 +0100

    added further tests to backstage passes

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -54,7 +54,8 @@ class GildedRoseTest {
         @BeforeEach
         void Setup(){
             items = new Item[]{
-                    new Item("Sulfuras, Hand of Ragnaros", 2, 80)
+                    new Item("Sulfuras, Hand of Ragnaros", 2, 80),
+                    new Item("Sulfuras, Hand of Ragnaros", 0, 80)
             };
             app = new GildedRose(items);
             app.updateQuality();
@@ -64,12 +65,14 @@ class GildedRoseTest {
         void QualityNeverChanges(){
             app.updateQuality();
             assertEquals(80, items[0].quality);
+            assertEquals(80, items[1].quality);
         }
 
         @Test
         void SellInNeverChanges(){
             app.updateQuality();
             assertEquals(2, items[0].sellIn);
+            assertEquals(0, items[1].sellIn);
         }
 
     }
@@ -88,6 +91,8 @@ class GildedRoseTest {
                     new Item("Backstage passes to a TAFKAL80ETC concert", 5, 20),
                     new Item("Backstage passes to a TAFKAL80ETC concert", 0, 20),
                     new Item("Backstage passes to a TAFKAL80ETC concert", -1, 20),
+                    new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+                    new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
             };
             app = new GildedRose(items);
             app.updateQuality();
@@ -127,6 +132,13 @@ class GildedRoseTest {
         @DisplayName("SellInDecreasesEachDay")
         void SellInChange(){
             assertEquals(14, items[0].sellIn);
+        }
+
+        @Test
+        @DisplayName("QualityNeverExceeds50")
+        void QualityMaximum(){
+            assertEquals(50, items[5].quality);
+            assertEquals(50, items[6].quality);
         }
 
     }
