e73f55f186b34988f8feb82a87caa0c6ec271811
Charlene <charlenechetcuti@gmail.com>
2018-06-20 23:29:29 +0100

    refactor backstage passes

--- src/main/java/com/gildedrose/Inn.java
+++ src/main/java/com/gildedrose/Inn.java
@@ -4,6 +4,7 @@ public class Inn {
 
     Item[] items;
     static final int MAXIMUM_QUALITY = 50;
+    static final int MINIMUM_QUALITY = 0;
 
     Inn(Item[] items) {
         this.items = items;
@@ -23,13 +24,17 @@ public class Inn {
     }
 
     void updateItemQuality(Item item){
-
+        int delta;
         switch (item.name) {
             case "Aged Brie":
-                increaseQuality(item, 1, 2 );
+                delta = getDelta(item, 1, 2);
+                increaseValue(item, delta);
                 break;
             case "Sulfuras, Hand of Ragnaros":
                 break;
+            case"Backstage passes to a TAFKAL80ETC concert":
+                delta = getBackStageDelta(item);
+                increaseValue(item, delta);
             default:
 
                 break;
@@ -37,14 +42,39 @@ public class Inn {
 
     }
 
-    void increaseQuality(Item item, int increaseAboveZeroSellIn, int increaseBelowZeroSellIn){
-            int delta = item.sellIn >= 0 ? increaseAboveZeroSellIn : increaseBelowZeroSellIn;
+    int getDelta(Item item, int AboveZeroSellIn, int BelowZeroSellIn){
+        return item.sellIn >= 0 ? AboveZeroSellIn : BelowZeroSellIn;
+    }
 
-            if (item.quality < MAXIMUM_QUALITY){
-                item.quality = (MAXIMUM_QUALITY - item.quality) >= delta ? item.quality + delta : MAXIMUM_QUALITY;
+    int getBackStageDelta(Item item){
+
+        if (item.sellIn <= 0 ){
+            return -item.quality;
+        }
+
+        else if (item.quality < MAXIMUM_QUALITY) {
+            if (item.sellIn > 10) {
+               return 1;
+            } else if (item.sellIn > 5) {
+               return 2;
+            } else {
+               return 3;
             }
+        }
+        else {
+           return 0;
+        }
 
     }
 
+    void increaseValue(Item item, int delta){
+
+        if (delta == -item.quality){
+            item.quality = 0;
+        }
+        else if (item.quality < MAXIMUM_QUALITY){
+            item.quality = (MAXIMUM_QUALITY - item.quality) >= delta ? item.quality + delta : MAXIMUM_QUALITY;
+        }
+    }
 
 }
--- src/test/java/com/gildedrose/InnTest.java
+++ src/test/java/com/gildedrose/InnTest.java
@@ -2,6 +2,7 @@ package com.gildedrose;
 
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
@@ -82,6 +83,77 @@ public class InnTest {
         }
 
     }
+
+    @Nested
+    class BackStagePasses{
+
+        private Item[] items;
+
+        @BeforeEach
+        void Setup(){
+
+            items = new Item[]{
+                    new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+                    new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20),
+                    new Item("Backstage passes to a TAFKAL80ETC concert", 5, 20),
+                    new Item("Backstage passes to a TAFKAL80ETC concert", 0, 20),
+                    new Item("Backstage passes to a TAFKAL80ETC concert", -1, 20),
+                    new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+                    new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+                    new Item("Backstage passes to a TAFKAL80ETC concert", 10, 50),
+                    new Item("Backstage passes to a TAFKAL80ETC concert", -1, 50),
+            };
+            app = new Inn(items);
+            app.updateQuality();
+        }
+
+        @Test
+        @DisplayName("QualityIncreasesBy1SellInGreaterThan10")
+        void QualityChange(){
+            assertEquals(21, items[0].quality);
+        }
+
+        @Test
+        @DisplayName("QualityIncreasesBy2SellInLessThan10GreaterThan5")
+        void QualityChange2(){
+            assertEquals(22, items[1].quality);
+        }
+
+        @Test
+        @DisplayName("QualityIncreasesBy3SellInLessThan5GreaterThan0")
+        void QualityChange3(){
+            assertEquals(23, items[2].quality);
+        }
+
+        @Test
+        @DisplayName("QualityIs0SellIn0")
+        void QualityChange4(){
+            assertEquals(0, items[3].quality);
+        }
+
+        @Test
+        @DisplayName("QualityIs0SellInLessThan0")
+        void QualityChange5(){
+            assertEquals(0, items[4].quality);
+            assertEquals(0, items[8].quality);
+        }
+
+        @Test
+        @DisplayName("SellInDecreasesEachDay")
+        void SellInChange(){
+            assertEquals(14, items[0].sellIn);
+        }
+
+        @Test
+        @DisplayName("QualityNeverExceeds50")
+        void QualityMaximum(){
+            assertEquals(50, items[5].quality);
+            assertEquals(50, items[6].quality);
+            assertEquals(50, items[7].quality);
+        }
+
+    }
+
 
 
 }
