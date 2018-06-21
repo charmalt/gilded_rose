3be7b81e1245098daefbcd66a45dcd2291fb5879
Charlene <charlenechetcuti@gmail.com>
2018-06-21 16:33:26 +0100

    rename feature test file

--- src/test/java/com/gildedrose/FeatureCoverageTest.java
+++ src/test/java/com/gildedrose/FeatureCoverageTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class InnTest {
+public class FeatureCoverageTest {
 
     private Inn app;
     private ItemExtended[] items;
